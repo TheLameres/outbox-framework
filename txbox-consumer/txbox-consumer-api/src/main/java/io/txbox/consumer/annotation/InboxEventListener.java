d3ea7487fa6799d3328f2447d32755b341bce091
TheLameres <lameres@yandex.ru>
2026-09-09 13:44:13 +0300

    Fix InboxEventListener

--- txbox-consumer/txbox-consumer-api/src/main/java/io/txbox/consumer/annotation/InboxEventListener.java
+++ txbox-consumer/txbox-consumer-api/src/main/java/io/txbox/consumer/annotation/InboxEventListener.java
@@ -1,5 +1,7 @@
 package io.txbox.consumer.annotation;
 
+import org.springframework.stereotype.Component;
+
 import java.lang.annotation.*;
 
 /**
@@ -24,6 +26,7 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
+@Component
 public @interface InboxEventListener {
 
     /**
